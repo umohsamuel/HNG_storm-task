3cf52268e1d40db08ec2afd6d050cb26898a0983
umohsamuel <65973850+umohsamuel@users.noreply.github.com>
2020-06-04 20:03:01 +0100

    Add files via upload

---
+++ testScripts/samuel_umoh.py
@@ -0,0 +1,5 @@
+def hng_intern(first_name, last_name, hng_id, language, email):
+    print(f"Hello World, this is {first_name} {last_name} with HNGi7 ID {hng_id} using {language} for stage 2 task. {email}")
+
+hng_intern('[Samuel]', '[Umoh]', '[HNG-06478]', '[Python]', 'samuelwisdom360@gmail.com')
+
